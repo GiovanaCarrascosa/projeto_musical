75b3d4dc7883e0ae48f3a86353e1dcee35f212da
eduarda virgilio <eduarda.virgilio2630@gmail.com>
2025-05-27 10:54:58 -0300

    produtos cadastrados e categorias aparecendo na tela principal

--- projeto_musical/model/controler_usuario.py
+++ projeto_musical/model/controler_usuario.py
@@ -4,8 +4,42 @@ from flask import session
 
 class Usuario:
     # cadastrando o usuario
-    def cadastrar_usuario():
-        pass
+    def cadastrar_usuario(usuario, email, nome, endereco, telefone, senha):
+        
+        #criptografando a senha
+
+        senha = sha256(senha.encode()).hexdigest()
+
+        #cadastrando as informações no banco de dados
+            
+        #criando a conexao
+            
+        conexao = Conexao.criar_conexao()
+            
+        #o cursor sera responsavel por manipular o banco de dados
+        cursor = conexao.cursor()
+
+        #criando o sql que sera executado
+            
+        sql = """INSERT INTO tb_usuarios (
+                        login, 
+                        senha,
+                        nome)
+                        
+                    VALUES (
+                        %s, %s, %s)"""
+                        
+        valores = (usuario, email, nome, endereco, telefone, senha)
+            
+        #executando o comando sql
+        cursor.execute(sql, valores)
+            
+        #confirmo a alteração
+        conexao.commit()
+            
+        #fecho a conexao com o banco
+        cursor.close()
+        conexao.close()
 
     # logando o usuario 
     def logar_usuario():
