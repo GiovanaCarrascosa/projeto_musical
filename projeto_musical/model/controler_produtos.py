de61325429d2367746157a060bb57b54d8429849
eduarda virgilio <eduarda.virgilio2630@gmail.com>
2025-06-07 17:06:28 -0300

    testes no main

--- projeto_musical/model/controler_produtos.py
+++ projeto_musical/model/controler_produtos.py
@@ -69,23 +69,26 @@ class Produtos:
     # selecionando os produtos da categoria cd/vinil
     def selecionar_categoria(codigo):
         #criando a conexao
-        
         conexao = Conexao.criar_conexao()
-
-        cursor = conexao.cursor(dictionary = True) 
+        cursor = conexao.cursor(dictionary=True) 
 
         sql = """
-                 select cod_produto, titulo, descricao, preco, cod_categoria from tb_produtos where cod_categoria = %s;
-                        """
+            SELECT tb_produtos.cod_produto,
+                tb_produtos.titulo,
+                tb_produtos.descricao,
+                tb_produtos.preco,
+                tb_produtos.cod_categoria,
+                tb_fotos_produto.url_foto
+            FROM tb_produtos
+            INNER JOIN tb_fotos_produto
+            ON tb_produtos.cod_produto = tb_fotos_produto.cod_produto
+            WHERE tb_produtos.cod_categoria = %s;
+        """
 
         valor = (codigo,)
-        #executando o comando sql
         cursor.execute(sql, valor)
-
-        #recuperando os dados e armazenando em uma variavel
         resultado = cursor.fetchall()
         
-        #fecho a conexao com o banco
         cursor.close()
         conexao.close()
 
