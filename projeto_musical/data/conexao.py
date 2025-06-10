995ec9c4bab25f1747abb67da33d6a4a3f100fa3
mariaebonifacio <madubonifacio12@gmail.com>
2025-06-10 08:54:16 -0300

    ADD: modificacoes

--- projeto_musical/data/conexao.py
+++ projeto_musical/data/conexao.py
@@ -4,11 +4,19 @@ class Conexao:
 
     def criar_conexao():
 
+            # conexao = mysql.connector.connect(
+            #     host="bdmusicalizando-musicalizando.j.aivencloud.com",
+            #     port= 28927,
+            #     user="avnadmin",
+            #     password="AVNS_qqBT8-vioTzb14zwf3o",
+            #     database = "defaultdb"
+            #     )
+            
             conexao = mysql.connector.connect(
-                host="bdmusicalizando-musicalizando.j.aivencloud.com",
-                port= 28927,
-                user="avnadmin",
-                password="AVNS_qqBT8-vioTzb14zwf3o",
-                database = "defaultdb"
+                host="localhost",
+                port= 3306,
+                user="root",
+                password="root",
+                database = "db_musicalizando"
                 )
             return conexao
