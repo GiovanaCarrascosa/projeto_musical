e2e0a08505f9df84dad8e72c0c78d905f6d2ab3c
eduarda virgilio <eduarda.virgilio2630@gmail.com>
2025-06-15 22:50:20 -0300

    pequeno ajuste na imagem

--- data/conexao.py
+++ data/conexao.py
@@ -8,7 +8,7 @@ class Conexao:
                 host="localhost",
                 port= 3306,
                 user="root",
-                password="12345",
+                password="root",
                 database = "db_musicalizando"
                 )
 
