fcf283e0a9e5ed6b335f6d31797f6dda16290994
Giovana Carrascosa <giovanaa.carrascosa194@gmail.com>
2025-06-16 17:24:17 -0300

    começando a fazer as correções das fotos na pagina de produtos

--- model/controler_produtos.py
+++ model/controler_produtos.py
@@ -50,12 +50,8 @@ class Produtos:
                    tb_produtos.titulo,
                    tb_produtos.descricao,
                    tb_produtos.preco,
-                   tb_produtos.cod_categoria,
-                   tb_fotos_produto.url_foto
-            FROM tb_produtos
-            INNER JOIN tb_fotos_produto
-            ON tb_produtos.cod_produto = tb_fotos_produto.cod_produto
-            WHERE tb_produtos.cod_produto = %s;
+                   tb_produtos.cod_categoria
+            FROM tb_produtos WHERE tb_produtos.cod_produto = %s;
         """
 
         valor = (codigo,)
@@ -72,6 +68,32 @@ class Produtos:
         conexao.close()
 
         return resultado
+    
+       # selecionando um produto
+    def selecionar_foto(codigo):
+        #criando a conexao
+        conexao = Conexao.criar_conexao()
+        cursor = conexao.cursor(dictionary = True) 
+
+        sql = """
+            SELECT url_foto, cod_produto from tb_fotos_produto where cod_produto = %s;
+        """
+
+        valor = (codigo,)
+        #executando o comando sql
+        cursor.execute(sql, valor)
+
+        #recuperando os dados e armazenando em uma variavel
+        resultado = cursor.fetchall() 
+
+
+        
+        #fecho a conexao com o banco
+        
+        conexao.close()
+
+        return resultado
+
 
 
 
